f49d670e5d822afb24d96ef9520505b0379804ee
Stephen White <stephenawhite57@gmail.com>
2019-07-09 22:02:41 +0200

    fix dashes and add Claudian letters

--- model/utility/graphemeCharacterMap.php
+++ model/utility/graphemeCharacterMap.php
@@ -17,22 +17,22 @@
 */
 /**
 * @author      Stephen White  <stephenawhite57@gmail.com>
-* @copyright   Stephen White  <stephenawhite57@gmail.com>
-* @link        https://github.com/stevewh/readlatin
-* @version     0.1
+* @copyright   @see AUTHORS in repository root <https://github.com/readsoftware/read>
+* @link        https://github.com/readsoftware
+* @version     1.0
 * @license     @see COPYING in repository root or <http://www.gnu.org/licenses/>
 * @package     READ Research Environment for Ancient Documents
 */
 require_once (dirname(__FILE__) . '/../entities/Entity.php');
 //setup internal configuration to ensure that array key lookup works
-if (function_exists('iconv') && PHP_VERSION_ID < 50600)
+if (function_exists('iconv') && PHP_VERSION_ID < 50295)
 {
   // These are settings that can be set inside code
   iconv_set_encoding("internal_encoding", "UTF-8");
   iconv_set_encoding("output_encoding", "UTF-8");
   iconv_set_encoding("input_encoding", "UTF-8");
 }
-else if (PHP_VERSION_ID >= 50600)
+else if (PHP_VERSION_ID >= 50295)
 {
   ini_set('default_charset', 'UTF-8');
 }
@@ -51,175 +51,261 @@ $graphemeTypeTermIDMap = array(
 
 GLOBAL $graphemeCharacterMap;
 $graphemeCharacterMap = array(
-"0"=>array("srt"=>"700","ssrt"=>"700","typ"=>"N"),
-"½"=>array("srt"=>"705","ssrt"=>"705","typ"=>"N"),
-"1"=>array(
-    "srt"=>"710","ssrt"=>"710","typ"=>"N",
-    "0"=>array( "srt"=>"760","ssrt"=>"760","typ"=>"N",
-        "0"=>array( "srt"=>"780","ssrt"=>"780","typ"=>"N",
-            "0"=>array("srt"=>"790","ssrt"=>"790","typ"=>"N")))),
-"2"=>array("srt"=>"720","ssrt"=>"720","typ"=>"N",
-    "0"=>array("srt"=>"770","ssrt"=>"770","typ"=>"N")),
-"3"=>array("srt"=>"730","ssrt"=>"730","typ"=>"N",
-    "0"=>array("srt"=>"773","ssrt"=>"773","typ"=>"N")),
-"4"=>array("srt"=>"740","ssrt"=>"740","typ"=>"N",
-    "0"=>array("srt"=>"774","ssrt"=>"774","typ"=>"N")),
-"5"=>array("srt"=>"755","ssrt"=>"755","typ"=>"N",
-    "0"=>array("srt"=>"775","ssrt"=>"775","typ"=>"N")),
-"6"=>array("srt"=>"756","ssrt"=>"756","typ"=>"N",
-    "0"=>array("srt"=>"776","ssrt"=>"776","typ"=>"N")),
-"7"=>array("srt"=>"757","ssrt"=>"757","typ"=>"N",
-    "0"=>array("srt"=>"777","ssrt"=>"777","typ"=>"N")),
-"8"=>array("srt"=>"758","ssrt"=>"758","typ"=>"N",
-    "0"=>array("srt"=>"778","ssrt"=>"778","typ"=>"N")),
-"9"=>array("srt"=>"759","ssrt"=>"759","typ"=>"N",
-    "0"=>array("srt"=>"779","ssrt"=>"779","typ"=>"N")),
-"‧"=>array("srt"=>"800","ssrt"=>"800","typ"=>"P"),
-"×"=>array("srt"=>"801","ssrt"=>"801","typ"=>"P"),
-"∈"=>array("srt"=>"830","ssrt"=>"830","typ"=>"P"),
-"⌇"=>array("srt"=>"880","ssrt"=>"880","typ"=>"P"),
-"◊"=>array("srt"=>"885","ssrt"=>"885","typ"=>"I"),
-"◈"=>array("srt"=>"885","ssrt"=>"885","typ"=>"I"),//bark obstruction =>space
-"○"=>array("srt"=>"820","ssrt"=>"820","typ"=>"P"),
-"⊗"=>array("srt"=>"822","ssrt"=>"822","typ"=>"P"),
-"◎"=>array("srt"=>"823","ssrt"=>"823","typ"=>"P"),
-"◦"=>array("srt"=>"810","ssrt"=>"810","typ"=>"P"),
-"•"=>array("srt"=>"805","ssrt"=>"805","typ"=>"P"),
-"∙"=>array("srt"=>"804","ssrt"=>"804","typ"=>"I"),//
-"☒"=>array("srt"=>"845","ssrt"=>"845","typ"=>"P"),
-"☸"=>array("srt"=>"840","ssrt"=>"840","typ"=>"P"),
-"❀"=>array("srt"=>"851","ssrt"=>"851","typ"=>"P"),
-"⁝"=>array("srt"=>"806","ssrt"=>"806","typ"=>"P"),
-"⏑"=>array("srt"=>"807","ssrt"=>"807","typ"=>"P"),
-"⎼"=>array("srt"=>"808","ssrt"=>"808","typ"=>"P"),
-","=>array("srt"=>"850","ssrt"=>"850","typ"=>"P"),
-"–"=>array("srt"=>"890","ssrt"=>"890","typ"=>"P"),
-"—"=>array("srt"=>"890","ssrt"=>"890","typ"=>"P"),
-"|"=>array(
-    "|"=>array("srt"=>"870","ssrt"=>"870","typ"=>"I"),
-    "srt"=>"860","ssrt"=>"860","typ"=>"I"),
-"◯"=>array("srt"=>"821","ssrt"=>"821","typ"=>"I"),
-":"=>array("srt"=>"803","ssrt"=>"803","typ"=>"P"),
-"*"=>array("srt"=>"099","ssrt"=>"099","typ"=>"I"),
-"·"=>array("srt"=>"000","ssrt"=>"099","typ"=>"I"),
-"."=>array("srt"=>"189","ssrt"=>"189","typ"=>"I"),
-"°"=>array("srt"=>"195","ssrt"=>"195","typ"=>"I"),
-"?"=>array("srt"=>"990","ssrt"=>"990","typ"=>"O"),
-"+"=>array("srt"=>"953","ssrt"=>"953","typ"=>"O"),
-"-"=>array(
-    "-"=>array(
-        "-"=>array("srt"=>"954","ssrt"=>"954","typ"=>"O")),
-    "srt"=>"950","ssrt"=>"950","typ"=>"O"), // debug temp assiqnment remove after / clean up
-"#"=>array("srt"=>"956","ssrt"=>"956","typ"=>"O"),
-"…"=>array("srt"=>"955","ssrt"=>"955","typ"=>"O"),
-";’"=>array("srt"=>"851","ssrt"=>"851","typ"=>"p"),
-"a"=>array(
-    "̣"=>array("srt"=>"410","csrt"=>"a","typ"=>"I"),
-    "̂"=>array("srt"=>"410","csrt"=>"a","typ"=>"I"),
-    "srt"=>"410","csrt"=>"a","typ"=>"I"),
-"á"=>array("srt"=>"410","csrt"=>"a","typ"=>"I"),
-"â"=>array("srt"=>"410","csrt"=>"a","typ"=>"I"),
-"b"=>array(
-    "̣"=>array("srt"=>"420","csrt"=>"b","typ"=>"I"),
-    "srt"=>"420","csrt"=>"b","typ"=>"I"),
-"c"=>array(
-    "̣"=>array("srt"=>"430","csrt"=>"c","typ"=>"I"),
-    "srt"=>"430","csrt"=>"c","typ"=>"I"),
-"ĉ"=>array("srt"=>"430","csrt"=>"c","typ"=>"I"),
-"d"=>array(
-    "̣"=>array("srt"=>"440","csrt"=>"d","typ"=>"I"),
-    "srt"=>"440","csrt"=>"d","typ"=>"I"),
-"e"=>array(
-    "̂"=>array("srt"=>"450","csrt"=>"e","typ"=>"I"),
-    "̣"=>array("srt"=>"450","csrt"=>"e","typ"=>"I"),
-    "srt"=>"450","csrt"=>"e","typ"=>"I"),
-"é"=>array("srt"=>"450","csrt"=>"e","typ"=>"I"),
-"ê"=>array("srt"=>"450","csrt"=>"e","typ"=>"I"),
-"f"=>array(
-    "̣"=>array("srt"=>"460","csrt"=>"f","typ"=>"I"),
-    "srt"=>"460","csrt"=>"f","typ"=>"I"),
-"g"=>array(
-    "̣"=>array("srt"=>"470","csrt"=>"g","typ"=>"I"),
-    "srt"=>"470","csrt"=>"g","typ"=>"I"),
-"ĝ"=>array("srt"=>"470","csrt"=>"g","typ"=>"I"),
-"h"=>array(
-    "̣"=>array("srt"=>"480","csrt"=>"h","typ"=>"I"),
-    "srt"=>"480","csrt"=>"h","typ"=>"I"),
-"ĥ"=>array("srt"=>"480","csrt"=>"h","typ"=>"I"),
-"i"=>array(
-    "͞"=>array("srt"=>"490","csrt"=>"i","typ"=>"I"),
-    "̣"=>array("srt"=>"490","csrt"=>"i","typ"=>"I"),
-    "srt"=>"490","csrt"=>"i","typ"=>"I"),
-"í"=>array("srt"=>"490","csrt"=>"i","typ"=>"I"),
-"î"=>array("srt"=>"490","csrt"=>"i","typ"=>"I"),
-"ị"=>array("srt"=>"490","csrt"=>"i","typ"=>"I"),
-"ī"=>array("srt"=>"490","csrt"=>"i","typ"=>"I"),
-"j"=>array(
-    "̣"=>array("srt"=>"500","csrt"=>"j","typ"=>"I"),
-    "srt"=>"500","csrt"=>"j","typ"=>"I"),
-"ĵ"=>array("srt"=>"500","csrt"=>"j","typ"=>"I"),
-"k"=>array(
-    "̣"=>array("srt"=>"510","csrt"=>"k","typ"=>"I"),
-    "srt"=>"510","csrt"=>"k","typ"=>"I"),
-"l"=>array(
-    "̣"=>array("srt"=>"520","csrt"=>"l","typ"=>"I"),
-    "srt"=>"520","csrt"=>"l","typ"=>"I"),
-"ḷ"=>array("srt"=>"520","csrt"=>"l","typ"=>"I"),
-"ḷ"=>array("srt"=>"520","csrt"=>"l","typ"=>"I"),
-"m"=>array(
-    "̂"=>array("srt"=>"530","csrt"=>"m","typ"=>"I"),
-    "̣"=>array("srt"=>"530","csrt"=>"m","typ"=>"I"),
-    "srt"=>"530","csrt"=>"m","typ"=>"I"),
-"n"=>array(
-    "̣"=>array("srt"=>"540","csrt"=>"n","typ"=>"I"),
-    "srt"=>"540","csrt"=>"n","typ"=>"I"),
-"o"=>array(
-    "̂"=>array("srt"=>"550","csrt"=>"o","typ"=>"I"),
-    "̣"=>array("srt"=>"550","csrt"=>"o","typ"=>"I"),
-    "srt"=>"550","csrt"=>"o","typ"=>"I"),
-"ó"=>array("srt"=>"550","csrt"=>"o","typ"=>"I"),
-"ọ"=>array("srt"=>"550","csrt"=>"o","typ"=>"I"),
-"ô"=>array("srt"=>"550","csrt"=>"o","typ"=>"I"),
-"p"=>array(
-    "̣"=>array("srt"=>"560","csrt"=>"p","typ"=>"I"),
-    "srt"=>"560","csrt"=>"p","typ"=>"I"),
-"q"=>array(
-    "̣"=>array("srt"=>"570","csrt"=>"q","typ"=>"I"),
-    "srt"=>"570","csrt"=>"q","typ"=>"I"),
-"r"=>array(
-    "̣"=>array("srt"=>"580","csrt"=>"r","typ"=>"I"),
-    "srt"=>"580","csrt"=>"r","typ"=>"I"),
-"s"=>array(
-    "̣"=>array("srt"=>"590","csrt"=>"s","typ"=>"I"),
-    "srt"=>"590","csrt"=>"s","typ"=>"I"),
-"ŝ"=>array("srt"=>"590","csrt"=>"s","typ"=>"I"),
-"t"=>array(
-    "̣"=>array("srt"=>"600","csrt"=>"t","typ"=>"I"),
-    "srt"=>"600","csrt"=>"t","typ"=>"I"),
-"u"=>array(
-    "̂"=>array("srt"=>"610","csrt"=>"u","typ"=>"I"),
-    "̣"=>array("srt"=>"610","csrt"=>"u","typ"=>"I"),
-    "srt"=>"610","csrt"=>"u","typ"=>"I"),
-"ú"=>array("srt"=>"610","csrt"=>"u","typ"=>"I"),
-"û"=>array("srt"=>"610","csrt"=>"u","typ"=>"I"),
-"v"=>array(
-    "̂"=>array("srt"=>"620","csrt"=>"v","typ"=>"I"),
-    "̣"=>array("srt"=>"620","csrt"=>"v","typ"=>"I"),
-    "srt"=>"620","csrt"=>"v","typ"=>"I"),
-"w"=>array(
-    ""=>array("srt"=>"630","csrt"=>"w","typ"=>"I"),
-    "̣"=>array("srt"=>"630","csrt"=>"w","typ"=>"I"),
-    "srt"=>"630","csrt"=>"w","typ"=>"I"),
-"x"=>array(
-    "̣"=>array("srt"=>"640","csrt"=>"x","typ"=>"I"),
-    "srt"=>"640","csrt"=>"x","typ"=>"I"),
-"y"=>array(
-    "̣"=>array("srt"=>"650","csrt"=>"y","typ"=>"I"),
-    "srt"=>"650","csrt"=>"y","typ"=>"I"),
-"ŷ"=>array("srt"=>"650","csrt"=>"y","typ"=>"I"),
-"z"=>array(
-    "̣"=>array("srt"=>"660","csrt"=>"z","typ"=>"I"),
-    "srt"=>"660","csrt"=>"z","typ"=>"I"));
+	// Digits
+	"0"=>array("srt"=>"700","typ"=>"N"),
+	"½"=>array("srt"=>"705","typ"=>"N"),
+	"1"=>array("srt"=>"710","typ"=>"N"),
+	"2"=>array("srt"=>"720","typ"=>"N"),
+	"3"=>array("srt"=>"730","typ"=>"N"),
+	"4"=>array("srt"=>"740","typ"=>"N"),
+	"5"=>array("srt"=>"750","typ"=>"N"),
+	"6"=>array("srt"=>"760","typ"=>"N"),
+	"7"=>array("srt"=>"770","typ"=>"N"),
+	"8"=>array("srt"=>"780","typ"=>"N"),
+	"9"=>array("srt"=>"790","typ"=>"N"),
+	// Punctuation and Intra-word Punctuation
+	","=>array("srt"=>"800","typ"=>"P"),
+	"."=>array("srt"=>"810","typ"=>"P"),
+	";"=>array("srt"=>"820","typ"=>"P"),
+	":"=>array("srt"=>"830","typ"=>"P"),
+	"!"=>array("srt"=>"840","typ"=>"P"),
+	"?"=>array("srt"=>"850","typ"=>"P"),
+	"["=>array("srt"=>"860","typ"=>"P"), //Open - space before
+	"]"=>array("srt"=>"870","typ"=>"P"), //Close - space after
+	"("=>array("srt"=>"860","typ"=>"P"), //Open - space before
+	")"=>array("srt"=>"870","typ"=>"P"), //Close - space after
+	"«"=>array("srt"=>"880","typ"=>"P"), //Open - space before
+	"»"=>array("srt"=>"890","typ"=>"P"), //Close - space after
+	// Other Symbols
+	"+"=>array("srt"=>"900","typ"=>"O"), //Placeholder, not yet observed in data
+	"-"=>array("srt"=>"910","typ"=>"I"), //Placeholder, not yet observed in data
+	// Latin
+	"A"=>array("srt"=>"100","typ"=>"I"),
+	"B"=>array("srt"=>"110","typ"=>"I"),
+	"C"=>array("srt"=>"120","typ"=>"I"),
+	"D"=>array("srt"=>"130","typ"=>"I"),
+	"E"=>array("srt"=>"140","typ"=>"I"),
+	"F"=>array("srt"=>"150","typ"=>"I"),
+	"G"=>array("srt"=>"160","typ"=>"I"),
+	"H"=>array("srt"=>"170","typ"=>"I"),
+	"I"=>array("srt"=>"180","typ"=>"I"),
+	"J"=>array("srt"=>"190","typ"=>"I"),
+	"K"=>array("srt"=>"200","typ"=>"I"),
+	"L"=>array("srt"=>"210","typ"=>"I"),
+	"M"=>array("srt"=>"220","typ"=>"I"),
+	"N"=>array("srt"=>"230","typ"=>"I"),
+	"O"=>array("srt"=>"240","typ"=>"I"),
+	"P"=>array("srt"=>"250","typ"=>"I"),
+	"Q"=>array("srt"=>"260","typ"=>"I"),
+	"R"=>array("srt"=>"270","typ"=>"I"),
+	"S"=>array("srt"=>"280","typ"=>"I"),
+	"T"=>array("srt"=>"290","typ"=>"I"),
+	"U"=>array("srt"=>"300","typ"=>"I"),
+	"V"=>array("srt"=>"310","typ"=>"I"),
+	"W"=>array("srt"=>"320","typ"=>"I"),
+	"X"=>array("srt"=>"330","typ"=>"I"),
+	"Y"=>array("srt"=>"340","typ"=>"I"),
+	"Z"=>array("srt"=>"350","typ"=>"I"),
+	"a"=>array(
+	    "̣"=>array("srt"=>"105","csrt"=>"a","typ"=>"I"),
+        "̂"=>array("srt"=>"105","csrt"=>"a","typ"=>"I"),
+        "srt"=>"105","csrt"=>"a","typ"=>"I"),
+    "á"=>array("srt"=>"105","csrt"=>"a","typ"=>"I"),
+	"â"=>array("srt"=>"105","csrt"=>"a","typ"=>"I"),
+	"b"=>array(
+		"̣"=>array("srt"=>"115","csrt"=>"b","typ"=>"I"),
+		"srt"=>"115","typ"=>"I"),
+	"c"=>array(
+		"̣"=>array("srt"=>"125","csrt"=>"c","typ"=>"I"),
+        "srt"=>"125","typ"=>"I"),
+	"ĉ"=>array("srt"=>"125","csrt"=>"c","typ"=>"I"),
+	"d"=>array(
+		"̣"=>array("srt"=>"135","csrt"=>"c","typ"=>"I"),
+        "srt"=>"135","typ"=>"I"),
+	"e"=>array(
+		"̂"=>array("srt"=>"145","csrt"=>"e","typ"=>"I"),
+        "̣"=>array("srt"=>"145","csrt"=>"e","typ"=>"I"),
+        "srt"=>"145","csrt"=>"e","typ"=>"I"),
+	"é"=>array("srt"=>"145","csrt"=>"e","typ"=>"I"),
+	"ê"=>array("srt"=>"145","csrt"=>"e","typ"=>"I"),
+	"f"=>array(
+		"̣"=>array("srt"=>"155","csrt"=>"f","typ"=>"I"),
+		"srt"=>"155","typ"=>"I"),
+	"g"=>array(
+		"̣"=>array("srt"=>"165","csrt"=>"g","typ"=>"I"),
+		"srt"=>"165","typ"=>"I"),
+	"h"=>array(
+		"̣"=>array("srt"=>"175","csrt"=>"h","typ"=>"I"),
+		"srt"=>"175","typ"=>"I"),
+	"ĥ"=>array("srt"=>"175","csrt"=>"h","typ"=>"I"),
+	"i"=>array(
+		"͞"=>array("srt"=>"185","csrt"=>"i","typ"=>"I"),
+        "̣"=>array("srt"=>"185","csrt"=>"i","typ"=>"I"),
+        "srt"=>"185","csrt"=>"i","typ"=>"I"),
+	"í"=>array("srt"=>"185","csrt"=>"i","typ"=>"I"),
+	"î"=>array("srt"=>"185","csrt"=>"i","typ"=>"I"),
+	"ị"=>array("srt"=>"185","csrt"=>"i","typ"=>"I"),
+	"ī"=>array("srt"=>"185","csrt"=>"i","typ"=>"I"),
+	"j"=>array(
+		"̣"=>array("srt"=>"195","csrt"=>"j","typ"=>"I"),
+        "srt"=>"195","csrt"=>"j","typ"=>"I"),
+	"ĵ"=>array("srt"=>"195","csrt"=>"j","typ"=>"I"),
+	"k"=>array(
+		"̣"=>array("srt"=>"205","csrt"=>"k","typ"=>"I"),
+        "srt"=>"205","csrt"=>"k","typ"=>"I"),
+	"l"=>array(
+		"̣"=>array("srt"=>"215","csrt"=>"l","typ"=>"I"),
+        "srt"=>"215","csrt"=>"l","typ"=>"I"),
+	"ḷ"=>array("srt"=>"215","csrt"=>"l","typ"=>"I"),
+	"ḷ"=>array("srt"=>"215","csrt"=>"l","typ"=>"I"),
+	"m"=>array(
+		"̂"=>array("srt"=>"225","csrt"=>"m","typ"=>"I"),
+        "̣"=>array("srt"=>"225","csrt"=>"m","typ"=>"I"),
+        "srt"=>"225","csrt"=>"m","typ"=>"I"),
+	"n"=>array(
+        "̣"=>array("srt"=>"235","csrt"=>"n","typ"=>"I"),
+        "srt"=>"235","csrt"=>"n","typ"=>"I"),
+	"o"=>array(
+        "̂"=>array("srt"=>"245","csrt"=>"o","typ"=>"I"),
+        "̣"=>array("srt"=>"245","csrt"=>"o","typ"=>"I"),
+        "srt"=>"245","csrt"=>"o","typ"=>"I"),
+	"ó"=>array("srt"=>"245","csrt"=>"o","typ"=>"I"),
+	"ọ"=>array("srt"=>"245","csrt"=>"o","typ"=>"I"),
+	"ô"=>array("srt"=>"245","csrt"=>"o","typ"=>"I"),
+	"p"=>array(
+        "̣"=>array("srt"=>"255","csrt"=>"p","typ"=>"I"),
+        "srt"=>"255","csrt"=>"p","typ"=>"I"),
+	"q"=>array(
+        "̣"=>array("srt"=>"265","csrt"=>"q","typ"=>"I"),
+        "srt"=>"265","csrt"=>"q","typ"=>"I"),
+	"r"=>array(
+        "̣"=>array("srt"=>"275","csrt"=>"r","typ"=>"I"),
+        "srt"=>"275","csrt"=>"r","typ"=>"I"),
+	"s"=>array(
+        "̣"=>array("srt"=>"285","csrt"=>"s","typ"=>"I"),
+        "srt"=>"285","csrt"=>"s","typ"=>"I"),
+	"ŝ"=>array("srt"=>"285","csrt"=>"s","typ"=>"I"),
+	"t"=>array(
+        "̣"=>array("srt"=>"295","csrt"=>"t","typ"=>"I"),
+        "srt"=>"295","csrt"=>"t","typ"=>"I"),
+	"u"=>array(
+        "̂"=>array("srt"=>"305","csrt"=>"u","typ"=>"I"),
+        "̣"=>array("srt"=>"305","csrt"=>"u","typ"=>"I"),
+        "srt"=>"305","csrt"=>"u","typ"=>"I"),
+    "ú"=>array("srt"=>"305","csrt"=>"u","typ"=>"I"),
+    "û"=>array("srt"=>"305","csrt"=>"u","typ"=>"I"),
+	"ⅎ"=>array("srt"=>"305","csrt"=>"u","typ"=>"I"),
+	"Ⅎ"=>array("srt"=>"305","csrt"=>"u","typ"=>"I"),
+	"v"=>array(
+        "̂"=>array("srt"=>"315","csrt"=>"v","typ"=>"I"),
+        "̣"=>array("srt"=>"315","csrt"=>"v","typ"=>"I"),
+        "srt"=>"315","csrt"=>"v","typ"=>"I"),
+	"w"=>array(
+        ""=>array("srt"=>"325","csrt"=>"w","typ"=>"I"),
+        "̣"=>array("srt"=>"325","csrt"=>"w","typ"=>"I"),
+        "srt"=>"325","csrt"=>"w","typ"=>"I"),
+	"x"=>array(
+        "̣"=>array("srt"=>"335","csrt"=>"x","typ"=>"I"),
+        "srt"=>"335","csrt"=>"x","typ"=>"I"),
+	"y"=>array(
+        "̣"=>array("srt"=>"345","csrt"=>"y","typ"=>"I"),
+        "srt"=>"345","csrt"=>"y","typ"=>"I"),
+	"ŷ"=>array("srt"=>"345","csrt"=>"y","typ"=>"I"),
+	"z"=>array(
+        "̣"=>array("srt"=>"355","csrt"=>"z","typ"=>"I"),
+        "srt"=>"355","csrt"=>"z","typ"=>"I"),
+	// Greek
+	"Ἀ"=>array("srt"=>"400","typ"=>"I"),
+	"Α"=>array("srt"=>"400","typ"=>"I"),
+	"Β"=>array("srt"=>"410","typ"=>"I"),
+	"Γ"=>array("srt"=>"420","typ"=>"I"),
+	"Δ"=>array("srt"=>"430","typ"=>"I"),
+	"Ε"=>array("srt"=>"440","typ"=>"I"),
+	"Ζ"=>array("srt"=>"450","typ"=>"I"),
+	"Η"=>array("srt"=>"460","typ"=>"I"),
+	"Θ"=>array("srt"=>"470","typ"=>"I"),
+	"Ι"=>array("srt"=>"480","typ"=>"I"),
+	"Κ"=>array("srt"=>"185","typ"=>"I"),
+	"Λ"=>array("srt"=>"195","typ"=>"I"),
+	"Μ"=>array("srt"=>"205","typ"=>"I"),
+	"Ν"=>array("srt"=>"215","typ"=>"I"),
+	"Ξ"=>array("srt"=>"225","typ"=>"I"),
+	"Ο"=>array("srt"=>"235","typ"=>"I"),
+	"Π"=>array("srt"=>"245","typ"=>"I"),
+	"Ρ"=>array("srt"=>"255","typ"=>"I"),
+	"Σ"=>array("srt"=>"265","typ"=>"I"),
+	"Σ"=>array("srt"=>"275","typ"=>"I"),
+	"Τ"=>array("srt"=>"285","typ"=>"I"),
+	"Υ"=>array("srt"=>"295","typ"=>"I"),
+	"Φ"=>array("srt"=>"305","typ"=>"I"),
+	"Χ"=>array("srt"=>"315","typ"=>"I"),
+	"Ψ"=>array("srt"=>"325","typ"=>"I"),
+	"Ω"=>array("srt"=>"335","typ"=>"I"),
+	"ἀ"=>array("srt"=>"405","typ"=>"I"),
+	"α"=>array("srt"=>"405","typ"=>"I"),
+	"β"=>array("srt"=>"415","typ"=>"I"),
+	"γ"=>array("srt"=>"425","typ"=>"I"),
+	"δ"=>array("srt"=>"435","typ"=>"I"),
+	"ε"=>array("srt"=>"445","typ"=>"I"),
+	"ἔ"=>array("srt"=>"445","typ"=>"I"),
+	"ζ"=>array("srt"=>"455","typ"=>"I"),
+	"η"=>array("srt"=>"465","typ"=>"I"),
+	"θ"=>array("srt"=>"475","typ"=>"I"),
+	"ι"=>array("srt"=>"485","typ"=>"I"),
+	"ί"=>array("srt"=>"485","typ"=>"I"),
+	"κ"=>array("srt"=>"495","typ"=>"I"),
+	"λ"=>array("srt"=>"505","typ"=>"I"),
+	"μ"=>array("srt"=>"515","typ"=>"I"),
+	"ν"=>array("srt"=>"525","typ"=>"I"),
+	"ξ"=>array("srt"=>"535","typ"=>"I"),
+	"ο"=>array("srt"=>"545","typ"=>"I"),
+	"π"=>array("srt"=>"555","typ"=>"I"),
+	"ρ"=>array("srt"=>"565","typ"=>"I"),
+	"ς"=>array("srt"=>"575","typ"=>"I"),
+	"ϛ"=>array("srt"=>"575","typ"=>"I"),
+	"σ"=>array("srt"=>"585","typ"=>"I"),
+	"τ"=>array("srt"=>"595","typ"=>"I"),
+	"υ"=>array("srt"=>"605","typ"=>"I"),
+	"ῦ"=>array("srt"=>"605","typ"=>"I"),
+	"φ"=>array("srt"=>"615","typ"=>"I"),
+	"ό"=>array("srt"=>"615","typ"=>"I"),
+	"χ"=>array("srt"=>"625","typ"=>"I"),
+	"ψ"=>array("srt"=>"635","typ"=>"I"),
+	"ω"=>array("srt"=>"645","typ"=>"I"),
+	// Hebrew
+	"א"=>array("srt"=>"345","typ"=>"I"), //ALEF
+	"ב"=>array("srt"=>"345","typ"=>"I"), //BET
+	"ג"=>array("srt"=>"345","typ"=>"I"), //GIMEL
+	"ד"=>array("srt"=>"345","typ"=>"I"), //DALET
+	"ה"=>array("srt"=>"345","typ"=>"I"), //HE
+	"ו"=>array("srt"=>"345","typ"=>"I"), //VAV
+	"ז"=>array("srt"=>"345","typ"=>"I"), //ZAYIN
+	"ח"=>array("srt"=>"345","typ"=>"I"), //HET
+	"ט"=>array("srt"=>"345","typ"=>"I"), //TET
+	"י"=>array("srt"=>"345","typ"=>"I"), //YOD
+	"ך"=>array("srt"=>"345","typ"=>"I"), //KAF FINAL
+	"כ"=>array("srt"=>"345","typ"=>"I"), //KAF
+	"ל"=>array("srt"=>"345","typ"=>"I"), //LAMED
+	"ם"=>array("srt"=>"345","typ"=>"I"), //MEM FINAL
+	"מ"=>array("srt"=>"345","typ"=>"I"), //MEM
+	"ן"=>array("srt"=>"345","typ"=>"I"), //NUN FINAL
+	"נ"=>array("srt"=>"345","typ"=>"I"), //NUN
+	"ס"=>array("srt"=>"345","typ"=>"I"), //SAMEKH
+	"ע"=>array("srt"=>"345","typ"=>"I"), //AYIN
+	"ף"=>array("srt"=>"345","typ"=>"I"), //PE FINAL
+	"פ"=>array("srt"=>"345","typ"=>"I"), //PE
+	"ץ"=>array("srt"=>"345","typ"=>"I"), //TSADI FINAL
+	"צ"=>array("srt"=>"345","typ"=>"I"), //TSADI
+	"ק"=>array("srt"=>"345","typ"=>"I"), //QOF
+	"ר"=>array("srt"=>"345","typ"=>"I"), //RESH
+	"ש"=>array("srt"=>"345","typ"=>"I"), //SHIN
+	"ת"=>array("srt"=>"345","typ"=>"I"), //TAV
+
+	// Logographic signs
+	// "☧"=>array("srt"=>"430","typ"=>"L") //chi rho (Christogram)
+	// Functional symbols handled by parser
+	// § [ ] "
+);
 
 /**
 * getNextSegmentState - state engine for segmenting a stream of grapheme types
@@ -233,7 +319,7 @@ $graphemeCharacterMap = array(
 * S(C)→C(C)→CC(.)→CC.(VM)→CC.VM(~VM)→S
 * S(C)→C(V)→CV(~VM)→S
 * S(C)→C(V)→CV(VM)→VM(~VM)→S
-* S(C)→C(.)→C.(VM)→VM(~VM)→Si
+* S(C)→C(.)→C.(VM)→VM(~VM)→S
 * S(C)→C(.)→C.→S(~VM)
 * S(V)→V(~VM)→S
 * S(.)→.(~VM && ~V)→S
