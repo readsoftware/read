eb071f0f878518f157ff8451266d7073f45daedd
selaudin <a.selaudin@gmail.com>
2023-06-21 04:15:33 +0200

    Update coco, add timeout for flask

--- admin/module/coco.py
+++ admin/module/coco.py
@@ -5,22 +5,28 @@ from PIL import Image, ImageFile
 import requests
 from io import BytesIO
 import os
+from dotenv import load_dotenv
+import locale
+
+load_dotenv()
 
 conf = RQC.readConnConfig
-APP = os.environ.get('DB_SERVERNAME')
-conf['host'] = os.environ.get('DB_SERVERNAME')
-conf['user'] = os.environ.get('DB_USERNAME')
-conf['password'] = os.environ.get('DB_PASSWORD')
+conf['host'] = os.getenv('DB_SERVERNAME')
+APP = os.getenv('DB_SERVERNAME')
+conf['user'] = os.getenv('DB_USERNAME')
+conf['password'] = os.getenv('DB_PASSWORD')
+usedCategories = []
 
-print("APP: "+APP)
-print("HOST: "+conf['host'])
-print("USER: "+conf['user'])
-print("PASS: "+conf['password'])
+# print("APP: ", APP)
+# print("HOST: ", conf['host'])
+# print("USER: ", conf['user'])
+# print("PASS: ", conf['password'])
 
 def getCOCO(db):
     conf['database'] = db
     myRQC = RQC.ReadQueryCursor(conf)
     print(conf)
+
     myRQC.query(
         '''
     WITH tags as (
@@ -47,6 +53,7 @@ def getCOCO(db):
     SELECT *
     FROM tagWithAndWithoutChildren
         ''')
+
 
     children = myRQC.getRowsAsIndexMultiDict(myRQC.getColumnNames().index('trm_parent'))
 
@@ -142,13 +149,21 @@ def getCOCO(db):
         ''')
 
     print(myRQC.getResultCount())
-    # return your first five rows
-    # dataframe.head(30)
 
     result = myRQC.getRowsAsIndexMultiDict(myRQC.getColumnNames().index('img_url'))
-    output = json.dumps(result, indent=0, sort_keys=True, default=str, ensure_ascii=False)
-    res = json.loads(output)
-    # print(res["http://localhost/images/homer2/txt1/P.Corn.Inv.MSS.A.101.XIII.jpg"])
+    resultDump = json.dumps(result, indent=0, sort_keys=True, default=str, ensure_ascii=False)
+    res = json.loads(resultDump)
+
+    ''' Query the texts in Database'''
+    myRQC.query(
+    '''
+        SELECT txt_id, txt_ckn, txt_title, txt_ref, txt_image_ids
+        FROM text;
+    ''')
+
+    txt = myRQC.getRowsAsIndexMultiDict(myRQC.getColumnNames().index('txt_id'))
+    txtDump = json.dumps(txt, indent=0, sort_keys=True, default=str, ensure_ascii=False)
+    txtResult = json.loads(txtDump)
 
     ''' Create JSON file'''
     initialization = {
@@ -157,52 +172,106 @@ def getCOCO(db):
                 "id": 1,
                 "name": "Attribution-NonCommercial-ShareAlike License",
                 "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+            },
+            {
+                "id": 2,
+                "name": "CC-BY-NC 3.0.",
+                "url": "https://creativecommons.org/licenses/by/3.0/"
+            },
+            {
+                "id": 3,
+                "name": "CC-BY-NC 4.0.",
+                "url": "https://creativecommons.org/licenses/by/4.0/"
+            },
+            {
+                "id": 4,
+                "name": "Berliner Papyrusdatenbank CC-BY-NC 4.0.",
+                "url": "https://berlpap.smb.museum/nutzungshinweise/"
+            },
+            {
+                "id": 5,
+                "name": "Bibliothèque de Genève",
+                "url": "https://archives.bge-geneve.ch/n/conditions-d-utilisation/n:121"
+            },
+            {
+                "id": 6,
+                "name": "Uni-Hamburg",
+                "url": "https://digitalisate.sub.uni-hamburg.de/urheberrechts-und-nutzungshinweise/"
+            },
+            {
+                "id": 7,
+                "name": "Digital Bodleian CC-BY-NC 4.0.",
+                "url": "https://digital.bodleian.ox.ac.uk/terms/"
+            },
+            {
+                "id": 8,
+                "name": "Uni Heidelberg - Restrictive",
+                "url": "https://www.uni-heidelberg.de/fakultaeten/philosophie/zaw/papy/images.html"
+            },
+            {
+                "id": 9,
+                "name": "No lisence"
+            },
+            {
+                "id": 10,
+                "name": "PSI lisence",
+                "url": "http://www.psi-online.it/rightpermission"
+
             }
         ],
         "categories": [],
         "images": [],
-        "annotations": []
+        "annotations": [],
+        "texts": [],
+        "database": db
     }
-    # initialization = json.dumps(initialization, indent=0, sort_keys=True)
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
 
     ''' Images array '''
+    image_offset = 1
     for row in res:
-        # we replace 'localhost' with 'read' because the docker container can't access the localhost
-        if APP != 'localhost':
-            row1 = row.replace("localhost", "read")
-            response = requests.get(row1, headers)
-        else:
-            response = requests.get(row, headers)
-
+        # we replace 'localhost' with 'read' because the docker container can't access images through localhost
+        row11 = row  # create duplicate to make a request with another link
+        if APP == 'db':
+            row11 = row11.replace("localhost", "read")
+            row11 = row11.replace("https://app.d-scribes.philhist.unibas.ch", "http://read")
+        # print(row11)
+        response = requests.get(row11, headers=headers)
         if response.status_code == 200:
             img = Image.open(BytesIO(response.content))
             width, height = img.size
-            # print(width,height)
+            # print(width, height)
         else:
             width, height = 0, 0
-            # print(width,height)
+            # print(width, height)
+
+        # If an annotations image title has not a Name, replace with its url.
+        if res[row][0]['img_title'] is None:
+            res[row][0]['img_title'] = row
 
         output = {
+            "id": image_offset,
+            "bln_id": res[row][0]['bln_id'],
             "license": 1,
-            "img_url": row,
+            "img_url": row,  # row[16:] slice the 'http://localhost' part from the url
             "height": height,
             "width": width,
-            "id": res[row][0]['bln_id'],
-            "bln_id": res[row][0]['bln_id'],
             "file_name": res[row][0]['img_title'] + ".jpg",
             "date_captured": None
         }
+
+        # increase the img id by 1
+        image_offset += 1
 
         initialization['images'].append(output)
 
     print("Finish images")
 
     ''' Categories array'''
-    csv = pd.read_csv('./module/categories.csv')
+    csv = pd.read_csv('categories.csv')
 
     for index, row in csv.iterrows():
         output = {
@@ -211,14 +280,10 @@ def getCOCO(db):
             "supercategory": row["supercategory"]
         }
         initialization['categories'].append(output)
-
     print("Finish categories")
 
     ''' Annotations array '''
     index = 0
-
-    # outfile = open("result.json")
-    # data = json.load(outfile)
 
     def convert(string):
         li = list(string.split(","))
@@ -226,9 +291,13 @@ def getCOCO(db):
 
     for row in res.values():
         for row1 in row:
-            # print(row1['trm_ids'])
-
-            category = next(x for x in initialization['categories'] if x['name'] == row1['gra_grapheme'])
+            # go through all the categories and find the annotation's category(id,name,supercategory)
+            category = next((x for x in initialization['categories'] if x['name'] == row1['gra_grapheme']), {
+                "id": 0,
+                "name": 'default',
+                "supercategory": 'defaultCategory'
+            })
+            usedCategories.append(category)
 
             position = row1['seg_image_pos'].replace('{"(', '')
             position = position.replace(')\"}', '')
@@ -260,10 +329,16 @@ def getCOCO(db):
 
             index = index + 1
 
+            # find the img id from the image array, through bln_id
+            img_id = 0
+            for img in initialization['images']:
+                if img['bln_id'] == row1['bln_id']:
+                    img_id = img['id']
+
             output = {
                 "id": index,
                 "seg_id": row1['seg_id'],
-                "image_id": row1['bln_id'],
+                "image_id": img_id,
                 "category_id": category['id'],
                 "bbox": [
                     int(topleftX),
@@ -272,67 +347,93 @@ def getCOCO(db):
                     height
                 ],
                 "area": width * height,
+
                 "iscrowd": 1,
-                # "tags":{
-                #     "BaseType": [],
-                #     "FootMarkType": [],
-                #     "VowelType": []
-                # }
                 "tags": a
             }
 
             initialization['annotations'].append(output)
 
-    initialization = json.dumps(initialization, indent=0, sort_keys=True)
-    jsonFile = open("./coco.json", "w")
-    jsonFile.write(initialization)
-    jsonFile.close()
     print("Finished annotations")
 
+    ''' SORTING CATEGORIES ALPHABETICALLY '''
+    filteredUsedCategories = []
+    chars = []
+    for obj in usedCategories:
+        if obj not in filteredUsedCategories:
+            filteredUsedCategories.append(obj)
+            chars.append(obj['name'])
 
-def getFilteredCOCO(file="coco.json", listt=[]):
-    with open(file) as m:
-        # load json file
-        data = json.load(m)
-        # load again the json file as a temp structure to store the result
-        output = json.load(m)
-        # clear the 'annotation' list, so we can insert the filtered
-        # annotation
-        output['annotations'].clear()
+    # sort the characters in alphabetical order using python's locale library
+    locale.setlocale(locale.LC_ALL, "")
+    chars.sort(key=locale.strxfrm)
 
-        # iterate through all annotations
-        for i in data['annotations']:
-            # after each iteration assign an "exists" var as False, so
-            # while checking if some filter from listt exists, you assign
-            # it as true, so it breaks from the loop to not enter the same
-            # annotation again if for any case, also the other filters
-            # appear in the annotation
-            exists = False
-            # go through filters
-            for x in listt:
-                # assuming it is not the first iteration, if there was a match,
-                # break from loop, check the next annotation
-                if exists:
+    # reorder the categories in alphabetical order
+    orderedUsedCategories = []
+    for char in chars:
+        for charObj in filteredUsedCategories:
+            if char == charObj['name']:
+                orderedUsedCategories.append(charObj)
+
+    initialization['categories'] = []
+    # initialization['categories'].append(orderedUsedCategories)
+    for cat in orderedUsedCategories:
+        initialization['categories'].append(cat)
+
+    print("Finished filtering categories", len(orderedUsedCategories), orderedUsedCategories)
+
+    ''' Texts array '''
+    for texts in txtResult.values():
+        for texts1 in texts:
+            sliced = ''
+            txt_ckn = texts1['txt_ckn']
+
+            for i, v in enumerate(txt_ckn):
+                if v == '1' or v == '2' or v == '3' or v == '4' or v == '5' or v == '6' or v == '7' or v == '8' or v == '9' or v == '0':
+                    sliced = txt_ckn[i:i+5]  # +5 because text ckn is 5 digit long
                     break
-                else:
-                    for type in i['tags']:
-                        if not isinstance(i['tags'][type], list):
-                            i['tags'][type] = [i['tags'][type]]
-                        if x == i['tags'][type][0]:
-                            output['annotations'].append(i)
-                            exists = True
-                        # break from loop if one is already satisfied
-                        if exists:
-                            break
 
-        jsonFile = open("cocoFiltered.json", "w")
+            # print(sliced)
+            texts1['tm'] = sliced
+
+            # txtOutput = {
+            #     "txt_id": index,
+            #     "seg_id": row1['seg_id'],
+            #     "image_id": row1['bln_id'],
+            #     "category_id": category['id'],
+            # }
+
+            initialization['texts'].append(texts1)
+
+    initialization = json.dumps(initialization, indent=4, sort_keys=True, ensure_ascii=False)
+    jsonFile = open("./coco.json", "w", encoding='utf-8')
+    jsonFile.write(initialization)
+    jsonFile.close()
+    print("Finished texts")
+
+
+# not working properly, will be used for multi filtering
+def getFilteredCOCO(file="cocoShort.json", filter=[], listt=[]):
+    with open(file) as m:
+        data = json.load(m)
+        output = json.load(open(file))
+        output['annotations'].clear()
+        for i in data['annotations']:
+            # print(i['tags'][filter][0])
+            for x in list:
+                if x == i['tags'][filter][0]:
+                    print(i)
+                    output['annotations'].append(i)
+
+        # print(output)
+
+        jsonFile = open("./cocoShortFiltered.json", "w")
         out = json.dumps(output, indent=0, sort_keys=True)
         jsonFile.write(out)
         jsonFile.close()
-        print("done")
 
 
-def getFilteredCOCO_AND(file="coco.json", listt=[]):
+def getFilteredCOCO_AND(file="coco.json", listt=['bt2', 'ft3', '1']):
     with open(file) as m:
         # load json file
         data = json.load(m)
@@ -366,14 +467,14 @@ def getFilteredCOCO_AND(file="coco.json", listt=[]):
                 # append the annotation to an temp list
                 output['annotations'].append(i)
 
-        jsonFile = open("cocoFilteredAND.json", "w")
+        jsonFile = open("./cocoFilteredAND.json", "w")
         out = json.dumps(output, indent=0, sort_keys=True)
         jsonFile.write(out)
         jsonFile.close()
         print("done")
 
 
-def getFilteredCOCO_OR(file="coco.json", listt=[]):
+def getFilteredCOCO_OR(file="coco.json", listt=['bt3']):
     with open(file) as m:
         # load json file
         data = json.load(m)
@@ -408,7 +509,7 @@ def getFilteredCOCO_OR(file="coco.json", listt=[]):
                         if exists:
                             break
 
-        jsonFile = open("cocoFilteredOR.json", "w")
+        jsonFile = open("./cocoFilteredOR.json", "w")
         out = json.dumps(output, indent=0, sort_keys=True)
         jsonFile.write(out)
         jsonFile.close()
