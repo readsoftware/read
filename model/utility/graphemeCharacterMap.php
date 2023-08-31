04f95d78b5d66d3919f201ee29e3e2a0c3ca4623
Stephen White <stephenawhite57@gmail.com>
2023-08-31 09:21:30 +0200

    update lemmaVE menuing

--- model/utility/graphemeCharacterMap.php
+++ model/utility/graphemeCharacterMap.php
@@ -342,6 +342,7 @@ $graphemeCharacterMap = array(
     "srt"=>"600","ssrt"=>"600","typ"=>"C"),
 "ṣ"=>array(
     "̂"=>array("srt"=>"619","ssrt"=>"619","typ"=>"C"),
+    "̈"=>array("srt"=>"614","ssrt"=>"614","typ"=>"C"),
     "̄"=>array(        
         "̱"=>array("srt"=>"613","ssrt"=>"613","typ"=>"C"),
         "srt"=>"612","ssrt"=>"612","typ"=>"C"),
